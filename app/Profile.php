1541f03b4530317aa9d059c2c75aa391a315c7f7
mahdibaleghsefat <m.baleghsefat@gmail.com>
2019-05-30 16:45:50 +0430

    default image for profile and etc.

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/default.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
